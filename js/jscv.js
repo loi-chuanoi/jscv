4ec340ba6cf25099ec1cbd3fee1e17c17939d70a
Huy Nguyễn <ngxhuy89@gmail.com>
2014-08-13 23:00:46 +0700

    Tạo dự án

---
+++ js/jscv.js
@@ -0,0 +1,114 @@
+/*************************************************
+/ Create by Huy Nguyen<ngxhuy89@gmail.com>
+/ Simple lightway js JSCV[JavaScript-Controller - View]/
+/ July 2014 - of kkhk co.ltd project
+/ All Rights Reserved © Copyright huynguyen 2014
+/***************************************************/
+
+function _r(template_name,load_data,not_post_data,in_data){
+   return render(template_name,load_data,not_post_data,in_data);
+}
+// render từ ejs ra giao diện
+function render(template_name,load_data,not_post_data,in_data){
+    var out_data = {"config":config,"in_data":in_data}
+    if(load_data){
+    var modals = template_name.split("/");
+    var data = {};
+        data = controller.request(modals[0] + "." + modals[1],null,not_post_data)
+        out_data.data = data;
+    }
+    if(config.theme!=""){
+        var template_name = config.view +  "/" + config.theme + "/" + template_name;
+    }else{
+        var template_name = config.view +  "/" +template_name;
+    }
+    return (new EJS({"url":template_name}).render(out_data));
+}
+function render_tpl(template_name,load_data,not_post_data,in_data,tpl){
+    var out_data = {"config":config,"in_data":in_data}
+    if(load_data){
+        var modals = template_name.split("/");
+        var data = {};
+        data = controller.request(modals[0] + "." + modals[1],null,not_post_data)
+        out_data.data = data;
+    }
+    var template_name = config.view +  "/" +tpl;
+    if(config.theme!=""){
+        template_name = config.view +  "/" + config.theme + "/" + tpl;
+    }
+    return (new EJS({"url":template_name}).render(out_data));
+}
+
+var jsloop = 0;
+
+/**
+ *  Include file script
+ * @param file
+ */
+
+function include(file){
+    $.ajax({
+        async: false,
+        url:file,
+        dataType:"script",
+        cache:true
+    }).done(function(data){jsloop=0;}).fail(function( jqxhr, settings, exception ) {
+       jsloop = jsloop +1;
+       if(jsloop <= 5){
+           setTimeout(function(){include(file);},1000);
+       }else{
+       }
+    });
+}
+
+
+
+/*          */
+$(function(){
+    if(config.res.indexOf(".") > 0){
+        var strs = config.res.split("/");
+        strs  = strs[strs.length-1];
+        config.res = config.res.replace(strs,"");
+    }
+    if(config.bs.indexOf(".") > 0){
+        var strs = config.bs.split("/");
+        strs  = strs[strs.length-1];
+        config.bs = config.bs.replace(strs,"");
+    }
+    if(config.res.substr(config.res.length-1,1)== "/"){
+        config.res = config.res.substr(0,config.res.length-1);    
+    }
+    if(config.bs.substr(config.bs.length-1,1)== "/"){
+        config.bs = config.bs.substr(0,config.bs.length-1);
+    }
+    for(var local_lib in config.user_libs){
+
+              include(config.bs + "/libs/" + config.user_libs[local_lib] + ".js")
+
+    }
+    for(var vendor_lib in config.vendor){
+
+            include(config.bs  + "/" + config.vendor[vendor_lib] + ".js")
+
+    }
+    $.when(
+        include(config.res + "/js/ejs.js"),
+        include(config.res + "/js/view.js"),
+        include(config.bs + "/controllers/index.js"),
+        include(config.res + "/js/router.js")
+    ).done(function(){
+        $("body").append(_r(config.index,true));
+        $(document).delegate("[data-view]","click",function(){
+             config.currentview = $(this).attr("data-view");
+             var place = $(this).attr("data-place");
+              if(place.length != 0){
+                  config.currentplace = place;
+                  if($(this).attr("data-call-modal") == ""){
+                      $(place).html(_r(config.currentview,true,true));
+                  }else{
+                       $(place).html(_r(config.currentview));
+                  }
+              }
+        });
+    });
+});
---
+++ js/router.js
@@ -0,0 +1,142 @@
+/*************************************************
+/ Create by Huy Nguyen<ngxhuy89@gmail.com>
+/ Simple lightway js JSCV[JavaScript - Controller - View]/
+/ July 2014 - of kkhk co.ltd project
+/ All Rights Reserved © Copyright huynguyen 2014
+/***************************************************/
+var controller = {};
+controller.submit = function (target) {
+    if (target) {
+        var api = $(target).attr("data-submit");
+        var submit_data = {};
+        $(target).find("input")
+            .each(function (e, input) {
+                var input_name = $(input).attr("name");
+                if (!input_name) {
+                    input_name = $(input).attr("id");
+                }
+                if (input_name) {
+                    submit_data[input_name] = $(input).val();
+                }
+            });
+        submit_data.target = target;
+        this.request(api, submit_data);
+    }
+    return false;
+}
+controller.request = function (api, data, not_post_data) {
+    var r = {};
+    if (not_post_data) {
+        var v = api.split(".");
+        var m = v[0];
+        var a = v[1];
+        if (eval("window." + m + " == undefined")) {
+            $.when(
+                include("controllers/" + m + ".js")
+            ).done(function () {
+                    r = eval("m = new " + m + "();m." + a + "(" + JSON.stringify(data) + ");");
+                });
+        } else {
+            r = eval("m = new " + m + "();m." + a + "(" + JSON.stringify(data) + ");");
+        }
+        return r;
+    }
+    if (config.no_cookies) {
+        var token = localStorage.getItem("token");
+        if (data == null || data == undefined) {
+            data = {}
+        }
+        ;
+        if (token != null && token.length > 10) {
+            data.token = token;
+        }
+
+    }
+    $.ajax({
+        url: config.df_server + "/?call=" + api,
+        cache: false,
+        async: false,
+        type: "POST",
+        data: data
+    }).done(function (data) {
+        v = api.split(".");
+        m = v[0];
+        a = v[1];
+        if (eval("window." + m + " == undefined")) {
+            $.when(
+                include("controllers/" + m + ".js")
+            ).done(function () {
+                    r = eval("m = new " + m + "();m." + a + "(" + JSON.stringify(data) + ");");
+                });
+        } else {
+            r = eval("m = new " + m + "();m." + a + "(" + JSON.stringify(data) + ");");
+        }
+        return r;
+    }).error(function () {
+        v = api.split(".");
+        m = v[0];
+        a = v[1];
+        if (eval("window." + m + " == undefined")) {
+            $.when(
+                include("controllers/" + m + ".js")
+            ).done(function () {
+                    r = eval("m = new " + m + "();m." + a + "(" + JSON.stringify(data) + ");");
+                });
+        } else {
+            r = eval("m = new " + m + "();m." + a + "(" + JSON.stringify(data) + ");");
+        }
+        return r;
+    });
+}
+controller.call_api = function (api, in_data, in_func) {
+    var results;
+    if (config.no_cookies) {
+        var token = localStorage.getItem("token");
+        if (in_data == undefined || in_data == null) {
+            in_data = {}
+        };
+        if (token != null && token.length > 10) {
+            in_data.token = token;
+        }
+
+    }
+    $.ajax({
+        cache: false,
+        async: false,
+        dataType: "json",
+        url: config.df_server + "/?call=" + api,
+        method: "POST",
+        data: in_data
+    }).done(function (data) {
+        if (in_func != undefined) {
+            results = in_func(data, in_data);
+        }
+    });
+    return results;
+}
+controller.call_api_async = function (api, in_data, in_func) {
+    if (config.no_cookies) {
+        var token = localStorage.getItem("token");
+        if (in_data == undefined || in_data == null) {
+            in_data = {}
+        }
+        ;
+        if (token != null &&  token.length > 10) {
+            in_data.token = token;
+        }
+
+    }
+    $.ajax({
+        cache: false,
+        async: true,
+        dataType: "json",
+        url: config.df_server + "/?call=" + api,
+        method: "POST",
+        data: in_data
+    }).done(function (data) {
+        if (in_func != undefined) {
+            in_func(data, in_data);
+        }
+    });
+}
+window.controller = controller;
