605f01387cdda0c3d35d18568bcd6dadc391fb1d
Huy Nguyễn <huynguyen1059@gmail.com>
2015-04-25 09:03:57 +0700

    Fix lỗi thừa thư mục build Build ready

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$IGNORE_FOLDER = array(
+                                "test\\"
+                                );
+$IGNORE_FILE = array(
+                            "build.bat",
+                            "README.md",
+                            "build.php",
+                            "config.php"
+                            );	
